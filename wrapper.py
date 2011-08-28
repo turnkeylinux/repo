35304c186dbb4a67a6ec66af94fa27daf629d443
Alon Swartz <alon@turnkeylinux.org>
2011-08-28 17:07:58 +0300

    initial commit

---
+++ wrapper.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# Copyright (c) TurnKey Linux - all rights reserved
+"""Python project skeleton
+"""
+from os.path import *
+import pyproject
+
+class CliWrapper(pyproject.CliWrapper):
+    DESCRIPTION = __doc__
+    
+    INSTALL_PATH = dirname(__file__)
+
+    # this variable allows you to optionally specify the order commands
+    # are printed in Commands.usage().
+    #
+    # "" prints an newline in the usage order
+    COMMANDS_USAGE_ORDER = []
+
+if __name__ == '__main__':
+    CliWrapper.main()
