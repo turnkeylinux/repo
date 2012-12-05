b8edb3c9fbcc13816e16fba1e414f74466ff3377
Alon Swartz <alon@turnkeylinux.org>
2012-12-05 13:18:01 +0200

    updated copyright/license

--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
-# Copyright (c) TurnKey Linux - all rights reserved
-"""Python project skeleton
+# Copyright (c) TurnKey Linux - http://www.turnkeylinux.org
+#
+# This file is part of Repo
+#
+# Repo is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+
+"""Tools to index and create a Debian package repository
 """
 from os.path import *
 import pyproject
